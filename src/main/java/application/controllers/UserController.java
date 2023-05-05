561b0a52e7811f5c7f6887826f7d39c4eecd791d
Olega1981911 <oleg12a@gmail.com>
2023-05-05 20:11:04 +0300

    import code from another repository

---
+++ src/main/java/application/controllers/UserController.java
@@ -0,0 +1,69 @@
+package application.controllers;
+
+import application.models.User;
+import application.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+    private final UserServices userServices;
+
+    @Autowired
+    public UserController(UserServices userServices) {
+        this.userServices = userServices;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("users", userServices.findAll());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userServices.findOne(id));
+        return "/ ";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("users") User user) {
+        return "new";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id) {
+        model.addAttribute("users", userServices.findOne(id));
+        return "edit";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("users") @Valid User user,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "new";
+
+        userServices.saveUser(user);
+        return "redirect:/ ";
+    }
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("users") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") long id) {
+        if (bindingResult.hasErrors())
+            return "edit";
+
+        userServices.update(id, user);
+        return "redirect:/ ";
+    }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userServices.deleteUser(id);
+        return "redirect:/ ";
+    }
+}
---
+++ src/main/java/application/services/UserServices.java
@@ -0,0 +1,48 @@
+package application.services;
+
+
+import application.models.User;
+import application.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServices {
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserServices(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(long id) {
+        Optional<User> found = userRepository.findById(id);
+        return found.orElse(null);
+    }
+
+    @Transactional
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void update(long id, User updateUser) {
+        updateUser.setId(id);
+        userRepository.save(updateUser);
+    }
+
+    @Transactional
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+}
